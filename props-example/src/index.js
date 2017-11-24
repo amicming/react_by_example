3618d07e161c775cfe655dd1e44f59a2cbd9b27f
Amit Pandya <amicming@gmail.com>
2017-11-23 16:47:42 -0800

    added props example

--- props-example/src/index.js
+++ props-example/src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-class Hello extends React.Component {
-  render () {
-    return (
-      <div>Hello World!</div>
-    )
-  }
-}
-
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<App personName='Vasistha' />, document.getElementById('root'));
